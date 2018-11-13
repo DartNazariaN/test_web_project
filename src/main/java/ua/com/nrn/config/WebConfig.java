49d3a4362a989425b4abd5f8c4ab388534181998
DartNazariaN <nestoraknazik@gmail.com>
2018-11-13 17:16:31 +0200

    create a package with configuration classes

---
+++ src/main/java/ua/com/nrn/config/WebConfig.java
@@ -0,0 +1,21 @@
+package ua.com.nrn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@ComponentScan("ua.com.nrn")
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/pages/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
